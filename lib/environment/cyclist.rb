856ef97be10344f54bd8ddd6ddabc810e06e6b2b
Michaël Villeneuve <mvilleneuve.pro@gmail.com>
2019-11-27 08:24:46 +0100

    Making cyclist levels available outside the instance scope

--- lib/environment/cyclist.rb
+++ lib/environment/cyclist.rb
@@ -7,7 +7,11 @@ module Environment
     end
 
     def adjust_power_to(fame)
-      @power = { unknown: 50, local: 75, celebrity: 100 }[fame.to_sym] || rand(1...120)
+      @power = self.class.levels[fame.to_sym] || rand(1...120)
+    end
+
+    def self.levels
+      { unknown: 50, local: 75, celebrity: 100 }
     end
   end
 end
