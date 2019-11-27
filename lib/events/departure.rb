3b54e6067b2597a93522f3b9a844eec2f75f1475
Michaël Villeneuve <mvilleneuve.pro@gmail.com>
2019-11-27 10:53:20 +0100

    Improve departure event

--- lib/events/departure.rb
+++ lib/events/departure.rb
@@ -2,6 +2,25 @@ module Events
   class Departure < Base
     process do
       countdown
+
+      say '...'
+
+      ask 'The pack seems to have set a pretty slow pace, time to react.',
+          attack: attack,
+          wait: wait
+
+      launch TourmaletClimb
+    end
+
+    path(:attack) do
+      say "Speaker > ATTACK FROM #{player.name.upcase} AT THE FRONT OF THE PELOTON"
+      player.attack
+      sleep 1
+      say "Speaker > Oops, spoke too early #{player.name} has already been caught back !"
+    end
+
+    path(:wait) do
+      say 'Wise decision, a big climb is coming !'
     end
 
     def countdown
--- test/lib/events/departure_test.rb
+++ test/lib/events/departure_test.rb
@@ -3,9 +3,10 @@ require_relative '../../test_helper'
 module Events
   class DepartureTest < TestHelper
     setup do
-      game = create_game
+      @game = create_game
+      Console::Response.any_instance.unstub(:valid_answer?)
       Events::Departure.any_instance.stubs(:sleep)
-      @event = Departure.start(@game)
+      Events::TourmaletClimb.any_instance.stubs(:start)
     end
 
     test 'countdown counts from 3 to 1' do
@@ -15,6 +16,18 @@ module Events
       Events::Departure.any_instance.expects(:say).with("GOO !!!").once
       Events::Departure.any_instance.expects(:sleep).with(1).times(3)
 
+      Departure.new(@game).countdown
+    end
+
+    test 'attack makes player attack and fail' do
+      answer_with('attack')
+      @game.player.expects(:attack).once
+      Departure.start(@game)
+    end
+
+    test 'wait takes you to the col du tourmalet' do
+      answer_with('wait')
+      Departure.any_instance.expects(:launch).with(TourmaletClimb)
       Departure.start(@game)
     end
   end
